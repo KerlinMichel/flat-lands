829c42ea60f5088501f504e160d13987fd1e4734
Kerlin Michel <michelkerlin@gmail.com>
2017-03-08 20:39:52 -0500

    Changed license to follow libgdx license, whoops

--- core/src/com/dwarfmines/game/FlatLands.java
+++ core/src/com/dwarfmines/game/FlatLands.java
@@ -1,12 +1,12 @@
 package com.dwarfmines.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class FlatLands extends ApplicationAdapter {
+public class FlatLands extends Game {
 	SpriteBatch batch;
 	Texture img;
 	
